036c78943c87928e114c40b8a8a1733bc5f151bc
khalliid9 <kvadah9@gmail.com>
2024-12-13 09:45:36 +0300

    making contacts view ready to display contacts list

--- lib/contacts_view.dart
+++ lib/contacts_view.dart
@@ -1,16 +1,58 @@
+import 'package:contacts/CRUD/crud_service.dart';
 import 'package:flutter/material.dart';
-class ContactsView extends StatelessWidget {
+
+class ContactsView extends StatefulWidget {
   const ContactsView({super.key});
+
+  @override
+  State<ContactsView> createState() => _ContactsViewState();
+}
+
+class _ContactsViewState extends State<ContactsView> {
+  late final DbService dbService;
+  late final Stream<List<Contacts>> contactsStram;
+
+  @override
+  void initState() {
+    dbService = DbService();
+    contactsStram = dbService.contactsStreamer.stream;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Contacts"),
-        backgroundColor: const Color.fromARGB(255, 209, 127, 3),
-      ),
-      body: const Center(
-        child: Text("Contacts List"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Contacts"),
+          backgroundColor: const Color.fromARGB(255, 209, 127, 3),
+        ),
+        body: StreamBuilder(
+          stream: contactsStram,
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+                if (snapshot.hasData) {
+                  final allContacts = snapshot.data as List<Contacts>;
+                  if (allContacts.isEmpty) {
+                    return const Center(child: Text("No contacts Availabel"));
+                  }
+                  return ListView.builder(
+                      itemCount: allContacts.length,
+                      itemBuilder: (context, index) {
+                        final contact = allContacts[index];
+                        return ListTile(
+                          title: Text(contact.name),
+                          subtitle: Text(contact.phone),
+                        );
+                      });
+                }
+                return const Text("no snapshot data");
+              default:
+                return const Text(
+                    'Connection state biether waiting nor active');
+            }
+          },
+        ));
   }
 }
