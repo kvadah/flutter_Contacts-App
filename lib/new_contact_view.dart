b100a0a6a77896a35b4e31fb0a2edd30bc780817
khalliid9 <kvadah9@gmail.com>
2024-12-14 09:25:13 +0300

    making new contacts page ready to accept new Contact

--- lib/new_contact_view.dart
+++ lib/new_contact_view.dart
@@ -38,97 +38,132 @@ class _NewContactState extends State<NewContact> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Contact'),
-        actions: [
-          TextButton(
-            onPressed: () async {},
-            child: const Text('Save'),
-          ),
-           TextButton(
-            onPressed: () async {},
-            child: const Text('Cancel'),
-          )
-        ],
       ),
       body: Container(
-  padding: const EdgeInsets.all(16.0),
-  color: Colors.black, // Black background
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      TextFormField(
-        style: const TextStyle(color: Colors.white), 
-        controller: nameController,// White input text
-        decoration: InputDecoration(
-          labelText: 'Name',
-          labelStyle: const TextStyle(color: Colors.white54), // Gray label
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.grey), // Gray boundary
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.white), // White on focus
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-      ),
-      const SizedBox(height: 20),
-      TextFormField(
-        style: const TextStyle(color: Colors.white),
-        controller: phoneController,
-        
-        decoration: InputDecoration(
-          labelText: 'Phone',
-          labelStyle: const TextStyle(color: Colors.white54),
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.grey),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.white),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-        keyboardType: TextInputType.phone,
-      ),
-      const SizedBox(height: 20),
-      TextFormField(
-        style: const TextStyle(color: Colors.white),
-        controller: emailController,
-        decoration: InputDecoration(
-          labelText: 'Email',
-          labelStyle: const TextStyle(color: Colors.white54),
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.grey),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.white),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-        keyboardType: TextInputType.emailAddress,
-      ),
-      const SizedBox(height: 20),
-      TextFormField(
-        style: const TextStyle(color: Colors.white),
-        controller: addressController,
-        decoration: InputDecoration(
-          labelText: 'Address',
-          labelStyle: const TextStyle(color: Colors.white54),
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.grey),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.white),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-      ),
-    ],
-  ),
-),
+        padding: const EdgeInsets.all(16.0),
+        color: Colors.black, // Black background
+        child: Column(
+          children: [
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              controller: nameController, // White input text
 
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[600],
+                labelText: 'Name',
+                labelStyle:
+                    const TextStyle(color: Colors.white54), // Gray label
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(color: Colors.black), // Gray boundary
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(color: Colors.white), // White on focus
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              controller: phoneController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[600],
+                prefixIcon: const Icon(Icons.phone),
+                labelText: 'Phone',
+                labelStyle: const TextStyle(color: Colors.white54),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              keyboardType: TextInputType.phone,
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              controller: emailController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[600],
+                prefixIcon: const Icon(Icons.email),
+                labelText: 'Email',
+                labelStyle: const TextStyle(color: Colors.white54),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              controller: addressController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[600],
+                labelText: 'Address',
+                labelStyle: const TextStyle(color: Colors.white54),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            Row(children: [
+              TextButton(
+                onPressed: () async {
+                  final name = nameController.text;
+                  final email = emailController.text;
+                  final phone = phoneController.text;
+                  final address = addressController.text;
+                  final contact = Contacts(
+                    name: name,
+                    email: email,
+                    phone: phone,
+                    address: address,
+                  );
+                  await widget.dbService.addContact(contact);
+                },
+                child: const Text(
+                  'Save',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {},
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ])
+          ],
+        ),
+      ),
     );
   }
 }
