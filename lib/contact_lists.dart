5b3dd02ea52ff3946c34e15da010ee86e5eefcef
khalliid9 <kvadah9@gmail.com>
2024-12-17 10:05:12 +0300

    create contact list view and return it in contacts view to make contacts view clean

---
+++ lib/contact_lists.dart
@@ -0,0 +1,69 @@
+import 'package:contacts/CRUD/crud_service.dart';
+import 'package:contacts/Utilities/colors.dart';
+import 'package:flutter/material.dart';
+
+class GroupedContactsList extends StatelessWidget {
+  final List<String> sortedKeys; // List of sorted letters for headers
+  final Map<String, List<Contacts>> groupedContacts;
+  const GroupedContactsList({
+    super.key,
+    required this.sortedKeys,
+    required this.groupedContacts,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: sortedKeys.length,
+      itemBuilder: (context, index) {
+        final letter = sortedKeys[index];
+        final contacts = groupedContacts[letter]!;
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Text(
+                letter,
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+            ...contacts.map((contact) {
+              final avatarColor = getRandomColor();
+              final initial =
+                  contact.name.isNotEmpty ? contact.name[0].toUpperCase() : "#";
+              return Container(
+                margin: const EdgeInsets.only(top: 5, left: 0.5, right: 0),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(166, 53, 44, 44),
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: SizedBox(
+                  height: 50,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: avatarColor,
+                      child: Text(initial),
+                    ),
+                    title: Text(
+                      contact.name,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            })
+          ],
+        );
+      },
+    );
+  }
+}
--- lib/contacts_view.dart
+++ lib/contacts_view.dart
@@ -1,5 +1,5 @@
 import 'package:contacts/CRUD/crud_service.dart';
-import 'package:contacts/Utilities/colors.dart';
+import 'package:contacts/contact_lists.dart';
 import 'package:contacts/new_contact_view.dart';
 import 'package:flutter/material.dart';
 
@@ -67,7 +67,9 @@ class _ContactsViewState extends State<ContactsView> {
                       a.name.toLowerCase().compareTo(b.name.toLowerCase()));
                   final Map<String, List<Contacts>> groupedContacts = {};
                   for (var contact in allContacts) {
-                    final firstLetter = contact.name.isNotEmpty
+                    final firstLetter = contact.name.isNotEmpty &&
+                            RegExp(r'^[A-Za-z]')
+                                .hasMatch(contact.name.trim()[0])
                         ? contact.name[0].toUpperCase()
                         : "#"; // Use '#' for names without valid letters
                     if (!groupedContacts.containsKey(firstLetter)) {
@@ -75,65 +77,17 @@ class _ContactsViewState extends State<ContactsView> {
                     }
                     groupedContacts[firstLetter]!.add(contact);
                   }
-                  final sortedKeys = groupedContacts.keys.toList()..sort();
+                  final sortedKeys = groupedContacts.keys.toList()
+                    ..sort((a, b) {
+                      if (a == '#') return 1;
+                      if (b == '#') return -1;
+                      return a.compareTo(b);
+                    });
                   if (allContacts.isEmpty) {
                     return const Center(child: Text("No contacts Availabel"));
                   }
-                  return ListView.builder(
-                    itemCount: sortedKeys.length,
-                    itemBuilder: (context, index) {
-                      final letter = sortedKeys[index];
-                      final contacts = groupedContacts[letter]!;
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 8),
-                            child: Text(
-                              letter,
-                              style: const TextStyle(
-                                color: Colors.grey,
-                                fontSize: 20,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                          ),
-                          ...contacts.map((contact) {
-                            final avatarColor = getRandomColor();
-                            final initial = contact.name.isNotEmpty
-                                ? contact.name[0].toUpperCase()
-                                : "#";
-                            return Container(
-                              margin: const EdgeInsets.only(
-                                  top: 5, left: 0.5, right: 0),
-                              decoration: BoxDecoration(
-                                color: const Color.fromARGB(166, 53, 44, 44),
-                                borderRadius: BorderRadius.circular(18.0),
-                              ),
-                              child: SizedBox(
-                                height: 50,
-                                child: ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundColor: avatarColor,
-                                    child: Text(initial),
-                                  ),
-                                  title: Text(
-                                    contact.name,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w300,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
-                          })
-                        ],
-                      );
-                    },
-                  );
+                  return GroupedContactsList(
+                      sortedKeys: sortedKeys, groupedContacts: groupedContacts);
                 }
                 return const Text("no snapshot data");
               default:
